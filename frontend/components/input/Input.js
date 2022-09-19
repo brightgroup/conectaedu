190c3c36289fafe887f4a919705bef7f33e380a9
andres10sa <camand10sa@gmail.com>
2022-09-19 08:16:24 -0500

    Fix loader and comments

--- frontend/components/input/Input.js
+++ frontend/components/input/Input.js
@@ -3,8 +3,8 @@ export const Input = ({ label = '', icon = 'fa-user', error = '', ...props }) =>
     <div className="mb-8">
       <div className="flex items-end gap-3 px-1 relative">
         <i className={`fa-solid ${icon} relative bottom-1 text-${error ? 'error' : 'blue'}`} />
-        <input className={`input border-b ${error ? 'text-error' : ''}`} required {...props} />
-        <label className={`input__label ${error ? 'input__label-error' : 'text-gray-500'}`}>{label}</label>
+        <input className={`input border-b ${error ? 'text-error' : ''}`} required {...props} placeholder={label} />
+        {/* <label className={`input__label ${error ? 'input__label-error' : 'text-gray-500'}`}>{label}</label> */}
       </div>
       {error && <small className="inline-block w-full text-left ml-8 text-tiny text-error">{error}</small>}
     </div>
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,16 @@
 import { Layout } from 'layout/Layout'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { setLoaderStatus } from 'redux/loader/actions'
 import { Home } from 'views/home'
 
 const Page = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(setLoaderStatus(false))
+  }, [])
+
   return (
     <Layout>
       <Home />
